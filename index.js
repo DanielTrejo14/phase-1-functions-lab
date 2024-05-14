0fcc33cec56caa1a9b956457133bdfff4c140eda
Daniel Trejo <dwtrejo14@comcast.net>
2024-05-14 15:46:45 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks < 42) {
+       return 42 - blocks;
+    } else {
+       return blocks - 42;
+    }
+ }
+
+    function distanceFromHqInFeet(x){
+        return distanceFromHqInBlocks(x) * 264;
+    }
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination)
+        return (start - destination) * 264
+    else if (start < destination)
+        return (destination - start) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+     const feet = distanceTravelledInFeet(start, destination);
+     if (feet <= 400)
+        return 0;
+    else if (feet > 400 && feet <= 2000) {
+        return (feet - 400) * 0.02
+    }
+    else if (feet > 2000 && feet <= 2500) 
+        return 25; 
+    else 
+        return 'cannot travel that far'
+}
